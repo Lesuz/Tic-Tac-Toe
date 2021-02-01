47fc8497e1bd1830b1c4a5a821f6c5dd135f25e6
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-02-01 19:35:40 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// component Square
+const Square = (props) => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <button 
+      className="square"
+      onClick={props.onClickEvent}
+    >
+      {props.value}
+    </button>
+  )
+}
+
+// component Board
+const Board = () => {
+
+  // Making an array which has a value of null for each square
+  const initialSquares = Array(9).fill(null);
+
+  const [squares, setSquares] = useState(initialSquares)
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClickEvent = (i) => {
+
+    // Make a copy of square state array
+    const newSquares = [...squares]
+
+
+    const winnerDeclared = Boolean(calculateWinner(newSquares))
+    const squareFilled = Boolean(newSquares[i])
+    if (winnerDeclared || squareFilled) {
+      return;
+    }
+
+    // Mutate the copy, setting the i-th element to 'X'
+    newSquares[i] = xIsNext ? 'X' : 'O'
+    // Call the setSquares function with the mutated copy
+    setSquares(newSquares)
+    // Set the displayd player sign to the other one
+    setXIsNext(!xIsNext)
+  }
+
+  // rendering each square with their own clickevent and value
+  const renderSquare = (i) => {
+    return(
+      <Square 
+        value={squares[i]}
+        onClickEvent={() => handleClickEvent(i)}
+      />
+    )
+  }
+
+  const winner = calculateWinner(squares)
+  const status = winner ?  
+    `Winner: ${winner}` :
+    `Next player: ${xIsNext ? 'X' : 'O'}`
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+      </div>
+    </div>
+  )
+}
+
+const Game = () => {
+  return (
+    <div className="game">
+      Tic-Tac-Toe
+      <Board/>
+    </div>
+  )
+}
+
+ReactDOM.render(<Game />, document.getElementById('root'))
+
+function calculateWinner(squares){
+
+  // all possible winning lines
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // winning rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // winning columns
+    [0, 4, 8], [2, 4, 6] // winning diagonials
+  ]
+
+  for(let line of lines) {
+    const [a, b, c] = line;
+    
+    if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a] // X or O
+    }
+  }
+
+  return null
+}
